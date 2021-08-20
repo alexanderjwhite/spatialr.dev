9bad0e39d37a9efdce87a085ba8b1a8281c4ac0e
White <whitealj@iu.edu>
2021-08-20 10:01:30 -0400

    faster implementation of distance matrix calculation

--- R/fct_dist_matrix.R
+++ R/fct_dist_matrix.R
@@ -5,27 +5,14 @@
 #'
 #' @return distance matrix
 #' @export
+#' 
+#' @import stats
 #'
 #' @examples fct_dist_matrix(matrix(rnorm(20), nrow = 10, ncol = 2), verbose = TRUE)
 fct_dist_matrix <- function(coords, verbose = TRUE){
   # ToDo: conditions for coord dataframe
   if(verbose){print("Computing Distance Matrix...")}
-  size <- nrow(coords)
-  w <- matrix(0, nrow = size, ncol = size)
-  red_size <- (size^2-size)/2
-  count <- 0
-  for(j in 2:size){
-    for(i in 1:(j-1)){
-      count <- count+1
-      if((count)%%(red_size/1e3)==0 & verbose){
-        cat("\r","")
-        cat("\r",paste0("Computing Distance Matrix: ", round((count/red_size),digits = 5)*100,"%"))
-        }
-      w[i,j] <- 1/norm(coords[i,]-coords[j,], type="2")
-    }
-  }
-  w <- w + t(w)
-  diag(w) <- 0
-  if(verbose){cat("\n","Done \n")}
+  w <- as.matrix(1/stats::dist(coords, upper = TRUE, diag = TRUE))
+  if(verbose){print("Done")}
   return(w)
 }
